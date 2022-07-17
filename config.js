626b2c1f4c0a5b3a2910bc3c2f4f89c508a7ad90
ytakun09 <109012188+ytakun09@users.noreply.github.com>
2022-07-17 22:45:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 let fs = require('fs')                    //CREATOR = GEMPY , DEVBOT
 let chalk = require('chalk')
 global.owner = [
-  ['6287898307350'],
-  ['66287898307350'],
-  ['6287898307350', 'gempy', true]  
+  ['6287819353757'],
+  ['6287819353757'],
+  ['6287819353757', 'gempy', true]  
   
 ] 
 global.ownername = [['Gempy']]
 global.namabot = [['PENTABOT']]
-global.ownerinsta = [['@gmpyyy']]
+global.ownerinsta = [['@rizmy.kk']]
 global.mods = [] 
 global.prems = [] 
 
